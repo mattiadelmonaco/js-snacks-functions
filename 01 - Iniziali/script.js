711ae2fef8ec3e3470a94bcec828706f5334c4e1
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-05 15:11:23 +0100

    init project

---
+++ 01 - Iniziali/script.js
@@ -0,0 +1,11 @@
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+
+
+// Dichiara la funzione qui.
+
+
+// Invoca la funzione qui e stampa il risultato in console
+
+
+
+//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
