628f1a9be9de6e3d169e0c315b518d2b6886d52b
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-05 18:19:50 +0100

    feat: add forth snack in folder4 and folder5

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -2,12 +2,18 @@
 
 const userName = 'Mario';
 
-
 // Dichiara la funzione qui.
 
+/**
+ * Function to say "hello" to a name
+ * 
+ * @param {string} name - name to say hello
+ * @returns {string}
+ */
+const helloName = (name) => `Ciao ${name}`
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(helloName("Mario"))
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -1,3 +1,26 @@
+// "saluti", risultato cartella quarto snack
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+// Dichiara la funzione qui.
+
+/**
+ * Function to say "hello" to a name
+ * 
+ * @param {string} name - name to say hello
+ * @returns {string}
+ */
+const helloName = (name) => `Ciao ${name}`
+
+// Invoca la funzione qui e stampa il risultato in console
+
+console.log(helloName("Mario"))
+
+//Risultato atteso se si passa 'Mario': // ciao Mario
+
+
+
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
 Il saluto deve essere :
 buongiorno se è mattina (fino alle 13), 
@@ -6,6 +29,7 @@ e buonasera se è sera (oltre le 17)
 */
 
 const name = 'Mario';
+
 
 
 // Dichiara la funzione qui.
