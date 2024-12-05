b235c34a4aba5bcdc33491bf3ccd3ed38d4ac220
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-05 16:18:01 +0100

    feat: add function

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,11 +1,28 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
+const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+console.log(names)
 
 // Dichiara la funzione qui.
 
+/**
+ * Get first character of an array
+ * 
+ * @param {Array} array - array's name
+ * @returns {String}
+ */
+
+const firstCharacter = (array) => {
+    let firstLetter = []
+    for (let i = 0; i < names.length; i++) {
+        firstLetter.push(names[i].substring(0, 1))
+    }
+    return firstLetter
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+firstLetter = firstCharacter(names)
+console.log(firstLetter)
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
