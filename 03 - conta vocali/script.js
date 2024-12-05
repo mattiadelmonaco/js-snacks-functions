2384db5312c081a7a328db4758f463349b52f5f1
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-05 18:02:41 +0100

    feat: add function

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -5,9 +5,27 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+/**
+ * How many vocals are in a word
+ * 
+ * @param {string} word - word to know how many vocals has
+ * @returns {Number}
+ */
+
+const howManyAndWhichVocalsAreIn = (word) => {
+    let vocalsFound = []
+    for (i = 0; i < word.length; i++) {
+        let letter = word[i].toUpperCase()
+     if (letter === "A" || letter === "E" || letter === "I" || letter === "O" || letter === "U") {
+        vocalsFound.push(letter)
+    }
+} return vocalsFound.length, vocalsFound
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const howManyVocals = howManyAndWhichVocalsAreIn ("javascript")
+console.log (howManyVocals)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
