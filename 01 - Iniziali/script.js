974ce4727cca602c69b42ea0a8bd89b4ebe39fa9
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-06 09:52:27 +0100

    fix: correction function

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -15,14 +15,13 @@ console.log(names)
 const getFirstCharacter = (array) => {
     let firstCharacter = []
     for (let i = 0; i < array.length; i++) {
-        firstCharacter.push(array[i].substring(0, 1))
+        firstCharacter.push(array[i][0])
     }
     return firstCharacter
 }
 
 // Invoca la funzione qui e stampa il risultato in console
 
-let firstLetter = getFirstCharacter(names)
-console.log(firstLetter)
+console.log(getFirstCharacter(names))
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
